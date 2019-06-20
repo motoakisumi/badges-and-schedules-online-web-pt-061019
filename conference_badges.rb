4c8ea7feeda0b3f17f78d19c581cd086cdc6f8f3
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-20 15:37:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+#names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array)
+  counter = 0
+  array.each do |name|
+    array[counter] = badge_maker(name)
+    counter +=1
+  end
+  return array
+end
+
+def assign_rooms(list)
+  new_array = []
+  list.each do |name|
+    index = list.index(name)
+    new_array[index] = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  return new_array
+end
+
+def printer (array)
+  new_array = batch_badge_creator(array) + assign_rooms(array)
+  
+  puts new_array
+end
+
